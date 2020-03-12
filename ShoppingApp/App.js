67bc32233b1778ba5b66a8cdd1973c1804821b8e
HuyLee24 <mihawk240600@gmail.com>
2020-03-12 19:47:24 +0200

    Add related files

--- ShoppingApp/App.js
+++ ShoppingApp/App.js
@@ -1,63 +1,45 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View} from 'react-native';
-import ListView from "deprecated-react-native-listview";
+import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Authentication from './Components/Authentication/Authentication';
+import ChangeInfo from './Components/ChangeInfo/ChangeInfo';
+import Main from './Components/Main/Main';
+import OrderHistory from './Components/OrderHistory/OrderHistory';
+import { Ionicons } from 'react-native-vector-icons';
+
+StatusBar.setHidden(true);
+
+const Tab = createBottomTabNavigator();
 
 export default class App extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-            dataSource: new ListView.DataSource({rowHasChanged:(r1,r2)=>r1!==r2})
-        }
-    }
 
     render (){
         return (
-            <View>
-                <Text style={styles.paragraph1}>
-                    Shopping App!
-                </Text>
-            <ListView 
-            dataSource={this.state.dataSource}
-            renderRow={(r)=>
-            <View style={styles.container}>              
-                <Text style={styles.paragraph2}>{r}</Text></View>
-            }
-            />
-            </View>
-          );
+            <NavigationContainer> 
+              <Tab.Navigator>
+                <Tab.Screen 
+                  name="Main" 
+                  component={Main} 
+                  options={{ 
+                    tabBarIcon: ({ color, size }) => (
+                      <Ionicons name="ios-home" color={color} size={size} />)
+                  }}
+                />
+                <Tab.Screen 
+                  name="Authentication" 
+                  component={Authentication} 
+                />
+                <Tab.Screen 
+                  name="ChangeInfo" 
+                  component={ChangeInfo} 
+                />
+                <Tab.Screen 
+                  name="OrderHistory" 
+                  component={OrderHistory} 
+                />
+              </Tab.Navigator>
+            </NavigationContainer>
+          )
     };
-
-    componentDidMount() {
-        //load data
-        var mang = ['Item1', 'Item2', 'Item3']
-        
-        this.setState({
-            dataSource: this.state.dataSource.cloneWithRows(mang)
-        });
-    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    margin: 5,
-  },
-  paragraph1: {
-    top: 10,
-    margin: 20,
-    fontSize: 26,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#34495e',
-  },
-  paragraph2: {
-    top: 10,
-    margin: 20,
-    fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#34495e',
-  },
-});
